6e4a75065c4c3641896073e66ffdd56adffe9c5e
Noah2211 <noah.decroix3@gmail.com>
2022-02-25 16:13:57 +0100

    done

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+require __DIR__ . '/Config.php';
+require __DIR__ . '/DB_Connect.php';
 
 /**
  * Commencez par importer le fichier sql live.sql via PHPMyAdmin.
@@ -10,3 +12,58 @@
  *
  * ( PS: Sélectionnez, mais affichez le résultat à chaque fois ! ).
  */
+
+$stm = DB_Connect::dbConnect()->prepare("
+    SELECT * FROM user
+");
+
+$stm->execute();
+
+echo "<pre>";
+print_r($stm->fetchAll());
+echo "</pre>";
+
+$stm = DB_Connect::dbConnect()->prepare("
+    SELECT * FROM article
+");
+
+$stm->execute();
+
+echo "<pre>";
+print_r($stm->fetchAll());
+echo "</pre>";
+
+$stm = DB_Connect::dbConnect()->prepare("
+    SELECT username FROM user 
+    WHERE id = ANY (SELECT user_fk FROM article WHERE contenu LIKE '%poterie%')
+");
+
+$stm->execute();
+
+echo "<pre>";
+print_r($stm->fetchAll());
+echo "</pre>";
+
+
+$stm = DB_Connect::dbConnect()->prepare("
+    SELECT username FROM user
+    WHERE id = ANY(SELECT user_fk FROM article GROUP BY user_fk HAVING count(user_fk) > 1)
+");
+
+$stm->execute();
+
+echo "<pre>";
+print_r($stm->fetchAll());
+echo "</pre>";
+
+
+$stm = DB_Connect::dbConnect()->prepare("
+    SELECT username FROM user WHERE username LIKE 'jane%'
+        AND id = ALL(SELECT user_fk FROM article)
+");
+
+$stm->execute();
+
+echo "<pre>";
+print_r($stm->fetchAll());
+echo "</pre>";
